52ee09f24e44ee98d9e42c2658da0d0968cfe7c0
keinakano415 <keinakano415@gmail.com>
2022-09-06 05:02:59 +0900

    update hardhat configuration with gas reporter & hardhat etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+require("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      }
+    },
+  },
+  gasReporter: {
+    enabled: process.env.GAS_REPORT !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.MOONSCAN_API_KEY,
+  }
 };
