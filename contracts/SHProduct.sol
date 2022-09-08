89bb19980b20b3c2b5095b8403bdb4df5e4c6de4
keinakano415 <keinakano415@gmail.com>
2022-09-09 05:49:45 +0900

    add deposit func into the structured product contract

--- contracts/SHProduct.sol
+++ contracts/SHProduct.sol
@@ -1,10 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-contract ShProduct {
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract ShProduct is ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
     address public immutable USDC;
 
     constructor(address _usdc) {
         USDC = _usdc;
+    }
+
+    /**
+     * @notice Deposits the USDC into the structured product and mint ERC1155 NFT
+     * @param _amount is the amount of USDC to deposit
+     */
+    function deposit(uint256 _amount) external nonReentrant {
+        IERC20(USDC).safeTransfer(address(this), _amount);
     }
 }
