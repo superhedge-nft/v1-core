8834b0668699d15b07e74510be98b9c9b89f23a0
keinakano415 <keinakano415@gmail.com>
2022-09-14 04:57:14 +0900

    update ShProduct & unit test

--- contracts/SHProduct.sol
+++ contracts/SHProduct.sol
@@ -4,21 +4,52 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./interfaces/IShNFT.sol";
+import "./ShNFT.sol";
 
 contract ShProduct is ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     address public immutable USDC;
+    address public deribitWallet;
 
-    constructor(address _usdc) {
+    IShNFT public shNFT;
+
+    event ShNFTCreated(
+        address indexed _product,
+        address indexed _shNFT,
+        string _name,
+        string _symbol
+    );
+
+    event Deposit(
+        address _user,
+        uint256 _amount
+    );
+
+    constructor(
+        address _usdc, 
+        address _deribitWallet, 
+        string memory _name,
+        string memory _symbol
+    ) {
         USDC = _usdc;
+        deribitWallet = _deribitWallet;
+
+        ShNFT _shNFT = new ShNFT(_name, _symbol);
+        shNFT = IShNFT(address(_shNFT));
+
+        emit ShNFTCreated(address(this), address(_shNFT), _name, _symbol);
     }
 
     /**
      * @notice Deposits the USDC into the structured product and mint ERC1155 NFT
      * @param _amount is the amount of USDC to deposit
      */
-    function deposit(uint256 _amount) external nonReentrant {
+    function deposit(uint256 _amount) external nonReentrant  {
+        require(_amount > 0, "invalid amount");
         IERC20(USDC).safeTransfer(address(this), _amount);
+
+        emit Deposit(msg.sender, _amount);
     }
 }
--- test/ShProduct.js
+++ test/ShProduct.js
@@ -4,12 +4,18 @@ const { ethers } = require("hardhat");
 describe("ShProduct", function () {
   let mockUSDC, shProduct;
   before(async () => {
+    const [owner, deribit, otherAccount] = await ethers.getSigners();
     const MockUSDC = await ethers.getContractFactory("MockUSDC");
     mockUSDC = await MockUSDC.deploy();
     await mockUSDC.deployed();
 
     const ShProduct = await ethers.getContractFactory("ShProduct");
-    shProduct = await ShProduct.deploy(mockUSDC.address);
+    shProduct = await ShProduct.deploy(
+      mockUSDC.address,
+      deribit.address,
+      "Superhedge NFT",
+      "SHN"
+    );
     await shProduct.deployed();
 
     console.log(shProduct.address);
