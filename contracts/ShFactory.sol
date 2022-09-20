e9e48f623d4f3297752eaee64e876d2b0f6c9c3e
keinakano415 <keinakano415@gmail.com>
2022-09-21 05:32:27 +0900

    modify factory contract

--- contracts/ShFactory.sol
+++ contracts/ShFactory.sol
@@ -2,6 +2,9 @@
 pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IShNFT.sol";
+import "./interfaces/IShProduct.sol";
+import "./ShNFT.sol";
 import "./ShProduct.sol";
 
 contract ShFactory is Ownable {
@@ -9,11 +12,27 @@ contract ShFactory is Ownable {
     mapping(string => address) public getProduct;
     address[] public products;
 
-    event ProductCreated(string _name, address indexed _product);
+    IShNFT public shNFT;
+
+    event ProductCreated(
+        string name, 
+        address indexed product,
+        uint256 tokenId,
+        uint256 maxSupply
+    );
+
+    constructor(
+        string memory _nftName,
+        string memory _nftSymbol
+    ) {
+        bytes32 salt = keccak256(abi.encodePacked(_nftName, _nftSymbol));
+
+        ShNFT _shNFT = new ShNFT{salt : salt}(_nftName, _nftSymbol);
+        shNFT = IShNFT(address(_shNFT));
+    }
 
     function createProduct(
         string memory _name,
-        string memory _symbol,
         string memory _underlying,
         address _qredo_derebit,
         uint256 _coupon,
@@ -21,14 +40,14 @@ contract ShFactory is Ownable {
         uint256 _strikePrice2,
         uint256 _issuanceDate,
         uint256 _maturityDate,
-        uint256 _maxCapacity
+        uint256 _maxCapacity,
+        IShNFT _shNFT
     ) external onlyOwner {
         require(getProduct[_name] == address(0), "Product already exists");
         bytes32 salt = keccak256(abi.encodePacked(_name));
 
         address productAddr = address(new ShProduct{salt:salt}(
             _name,
-            _symbol,
             _underlying,
             _qredo_derebit,
             _coupon,
@@ -36,13 +55,18 @@ contract ShFactory is Ownable {
             _strikePrice2,
             _issuanceDate,
             _maturityDate,
-            _maxCapacity
+            _maxCapacity,
+            _shNFT
         ));
 
         getProduct[_name] = productAddr;
         products.push(productAddr);
         
-        emit ProductCreated(_name, productAddr);
+        uint256 maxSupply = _maxCapacity / (1000 * 1 ether);
+        uint256 tokenId = shNFT.mint(productAddr, maxSupply, "");
+        IShProduct(productAddr).setTokenId(tokenId);
+        
+        emit ProductCreated(_name, productAddr, tokenId, maxSupply);
     }
 
     function numOfProducts() external view returns (uint256) {
