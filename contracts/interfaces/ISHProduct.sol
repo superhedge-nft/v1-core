9e4dc1ac9a9b991d5f6e35219fd82e460a54b929
smartcoding51 <keinakano415@gmail.com>
2022-09-28 04:22:18 +0900

    update SHProduct interface

--- contracts/interfaces/ISHProduct.sol
+++ contracts/interfaces/ISHProduct.sol
@@ -2,7 +2,22 @@
 pragma solidity 0.8.9;
 
 interface ISHProduct {
-    function deposit(uint256 _amount) external;
+    /// @notice Struct representing issuance cycle
+    struct IssuanceCycle {
+        uint256 coupon;
+        uint256 strikePrice1;
+        uint256 strikePrice2;
+        uint256 issuanceDate;
+        uint256 maturityDate;
+    }
 
-    function setTokenId(uint256 _tokenId) external;
+    /// @notice Enum representing product status
+    enum Status {
+        Accepted,
+        Locked,
+        Issued,
+        Mature
+    }
+
+    function deposit(uint256 _amount) external;
 }
