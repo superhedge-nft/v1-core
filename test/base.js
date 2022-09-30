1a68371e53e2154eb900e7213a6e780480dc1588
smartcoding51 <keinakano415@gmail.com>
2022-10-01 02:26:28 +0900

    modify unit test script

--- test/base.js
+++ test/base.js
@@ -8,28 +8,31 @@ describe("ShFactory test suite", function () {
     [owner, qredoDeribit] = await ethers.getSigners();
 
     const SHFactory = await ethers.getContractFactory("SHFactory");
-    shFactory = await SHFactory.deploy(
-      "Superhedge NFT",
-      "SHN"
-    );
+    shFactory = await SHFactory.deploy();
     await shFactory.deployed();
+
+    const SHNFT = await ethers.getContractFactory("SHNFT");
+    shNFT = await SHNFT.deploy(
+      "Superhedge NFT", "SHN", shFactory.address
+    );
+    await shNFT.deployed();
   });
 
   it("Create product", async() => {
-    const shNFTAddr = await shFactory.shNFT();
     const productName = "BTC Defensive Spread";
 
     expect(await shFactory.createProduct(
       productName,
       "BTC/USD",
       qredoDeribit.address,
-      10, // 0.10% in basis points
-      25000,
-      20000,
-      1664640000,
-      1666972800,
+      shNFT.address,
       1000000,
-      shNFTAddr
+      {
+        coupon: 10, // 0.10% in basis points
+        strikePrice1: 25000,
+        strikePrice2: 20000,
+        uri: ""
+      }
     )).to.be.emit(shFactory, "ProductCreated");
 
     expect(await shFactory.numOfProducts()).to.equal(1);
@@ -39,9 +42,5 @@ describe("ShFactory test suite", function () {
     console.log(`SHProduct contract deployed at ${productAddr}`);
     const SHProduct = await ethers.getContractFactory("SHProduct");
     shProduct = SHProduct.attach(productAddr);
-    
-    const SHNFT = await ethers.getContractFactory("SHNFT");
-    shNFT = SHNFT.attach(shNFTAddr);
-    expect(await shProduct.tokenId()).to.equal(await shNFT.getCurrentTokenID());
   });
 });
