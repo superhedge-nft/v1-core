1fc136d045b4199d07505a08ed8caa6bde8e6153
smartcoding51 <keinakano415@gmail.com>
2022-10-05 03:36:58 +0900

    add mock contracts for unit testing

---
+++ contracts/mocks/MockFactory.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "../interfaces/ISHNFT.sol";
+import "../interfaces/ISHProduct.sol";
+import "./MockProduct.sol";
+
+/**
+ * @notice factory contract to create new products(vaults)
+ */
+contract MockFactory is Ownable {
+    /// @notice mapping from product name to product address 
+    mapping(string => address) public getProduct;
+    /// @notice Boolean check for if an address is an instrument
+    mapping(address => bool) public isProduct;
+    /// @notice array of products' addresses
+    address[] public products;
+    /// @notice ERC1155 NFT contract address
+
+    event ProductCreated(
+        string name, 
+        string underlying,
+        address indexed product,
+        uint256 maxSupply
+    );
+
+    event IssuanceCycleSet(
+        address indexed product,
+        uint256 coupon,
+        uint256 strikePrice1,
+        uint256 strikePrice2
+    );
+
+    /**
+     * @notice function to create new product(vault)
+     * @param _name is the product name
+     * @param _underlying is the underlying asset label
+     * @param _qredo_deribit is the wallet address of Deribit trading platform
+     * @param _maxCapacity is the maximum USDC amount that this product can accept
+     * @param _issuanceCycle is the struct variable with issuance date, 
+        maturiy date, coupon, strike1 and strke2
+     */
+    function createProduct(
+        string calldata _name,
+        string calldata _underlying,
+        address _qredo_deribit,
+        address _shNFT,
+        uint256 _maxCapacity,
+        ISHProduct.IssuanceCycle calldata _issuanceCycle        
+    ) external onlyOwner {
+        require(getProduct[_name] == address(0), "Product already exists");
+        require((_maxCapacity % 1000) == 0, "Max capacity must be whole-number thousands");
+
+        bytes32 salt = keccak256(abi.encodePacked(_name));
+        // create new product contract
+        address productAddr = address(new MockProduct{salt:salt}(
+            _name,
+            _underlying,
+            _qredo_deribit,
+            _shNFT,
+            _maxCapacity,
+            _issuanceCycle
+        ));
+
+        getProduct[_name] = productAddr;
+        isProduct[productAddr] = true;
+        products.push(productAddr);
+        
+        ISHNFT(_shNFT).addMinter(productAddr);
+        _setIssuanceCycle(productAddr, _issuanceCycle);
+        
+        emit ProductCreated(_name, _underlying, productAddr, _maxCapacity);
+    }
+
+    function setIssuanceCycle(
+        address _product, 
+        ISHProduct.IssuanceCycle calldata _issuanceCycle
+    ) external onlyOwner {
+        _setIssuanceCycle(_product, _issuanceCycle);
+    }
+
+    function _setIssuanceCycle(
+        address _product,
+        ISHProduct.IssuanceCycle memory _issuanceCycle
+    ) internal {
+
+        address shNFT = ISHProduct(_product).shNFT();
+        ISHNFT(shNFT).tokenIdIncrement();
+        uint256 tokenId = ISHNFT(shNFT).currentTokenID();
+
+        /* if (bytes(_issuanceCycle.uri).length != 0) {
+            ISHNFT(shNFT).setTokenURI(tokenId, _issuanceCycle.uri);
+        } */
+
+        ISHProduct(_product).setCurrentTokenId(tokenId);
+        ISHProduct(_product).setIssuanceCycle(_issuanceCycle);
+
+        emit IssuanceCycleSet(
+            _product,
+            _issuanceCycle.coupon, 
+            _issuanceCycle.strikePrice1, 
+            _issuanceCycle.strikePrice2
+        );
+    }
+
+    /**
+     * @notice returns the number of products
+     */
+    function numOfProducts() external view returns (uint256) {
+        return products.length;
+    }
+}
---
+++ contracts/mocks/MockProduct.sol
@@ -0,0 +1,167 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "../interfaces/ISHProduct.sol";
+import "../interfaces/ISHNFT.sol";
+import "../interfaces/IUSDC.sol";
+import "../SHNFT.sol";
+
+contract MockProduct is ISHProduct, Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
+    string public name;
+    string public underlying;
+
+    address public USDC;
+    /// @notice role in charge of status transition automation: Gelato Ops
+    address public ops;
+    address public qredoDeribit;
+
+    uint256 public maxCapacity;
+    uint256 public currentTokenId;
+    uint256 public currentCapacity;
+
+    Status public status;
+    IssuanceCycle public issuanceCycle;
+
+    address public shNFT;
+
+    mapping(address => uint256) public balances;
+
+    event Deposit(
+        address _from,
+        uint256 _amount,
+        uint256 _currentTokenId,
+        uint256 _supply
+    );
+
+    event Withdraw(
+        address _to,
+        uint256 _amount,
+        uint256 _currentTokenId
+    );
+
+    /**
+     * @dev Throws if called by any account other than the keeper.
+     */
+    modifier onlyOps() {
+        require(msg.sender == ops, "OpsReady: onlyOps");
+        _;
+    }
+
+    modifier onlyAccepted() {
+        require(status == Status.Accepted, "Not accepted status");
+        _;
+    }
+
+    modifier notIssued() {
+        require(status != Status.Issued, "Issued status");
+        _;
+    }
+
+    constructor(
+        string memory _name,
+        string memory _underlying,
+        address _qredo_deribit,
+        address _shNFT,
+        uint256 _maxCapacity,
+        IssuanceCycle memory _issuanceCycle
+    ) {
+        name = _name;
+        underlying = _underlying;
+
+        qredoDeribit = _qredo_deribit;
+        maxCapacity = _maxCapacity;
+
+        shNFT = _shNFT;
+        issuanceCycle = _issuanceCycle;
+    }
+
+    function fundAccept() external onlyOps {
+        status = Status.Accepted;
+        currentCapacity = 0;
+    }
+
+    function fundLock() external onlyOps {
+        status = Status.Locked;
+    }
+
+    function issuance() external onlyOps {
+        status = Status.Issued;
+        issuanceCycle.issuanceDate = block.timestamp;
+    }
+
+    function mature() external onlyOps {
+        status = Status.Mature;
+        issuanceCycle.maturityDate = block.timestamp;
+    }
+
+    function setCurrentTokenId(uint256 _id) external {
+        currentTokenId = _id;
+    }
+
+    function setIssuanceCycle(
+        IssuanceCycle calldata _issuanceCycle
+    ) external onlyOwner notIssued {
+        issuanceCycle = _issuanceCycle;
+    }
+    
+    /**
+     * @dev Deposits the USDC into the structured product and mint ERC1155 NFT
+     * @param _amount is the amount of USDC to deposit
+     */
+    function deposit(uint256 _amount) external nonReentrant onlyAccepted {
+        require(_amount > 0, "Amount must be greater than zero");
+        uint256 decimals = IUSDC(USDC).decimals();
+        uint256 supply = _amount % (1000 * 10 ** decimals);
+        require(supply == 0, "Amount must be whole-number thousands");
+
+        uint256 prevTokenId = currentTokenId - 1;
+        uint256 prevBalance = ISHNFT(shNFT).balanceOf(msg.sender, prevTokenId);
+
+        currentCapacity += _amount;
+        require(maxCapacity >= currentCapacity, "Product is full");
+
+        IERC20(USDC).safeTransferFrom(msg.sender, address(this), _amount);
+
+        if (prevBalance > 0) {
+            ISHNFT(shNFT).burn(msg.sender, prevTokenId, prevBalance);
+            supply += prevBalance;
+        }
+
+        ISHNFT(shNFT).mint(msg.sender, currentTokenId, supply, issuanceCycle.uri);
+        balances[msg.sender] += _amount;
+        
+        emit Deposit(msg.sender, _amount, currentTokenId, supply);
+    }
+
+    /**
+     * @dev Withdraws the specific amount of USDC from the structured product
+     * @param _amount is the amount of USDC to withdraw
+     */
+    function withdraw(uint256 _amount) external nonReentrant onlyAccepted {
+        require(_amount > 0, "Amount must be greater than zero");
+        uint256 decimals = IUSDC(USDC).decimals();
+        uint256 supply = _amount % (1000 * 10 ** decimals);
+        require(supply == 0, "Amount must be whole-number thousands");
+        require(balances[msg.sender] >= _amount, "Exceeds current balance");
+
+        ISHNFT(shNFT).burn(msg.sender, currentTokenId, supply);
+        currentCapacity -= _amount;
+        balances[msg.sender] -= _amount;
+
+        emit Withdraw(msg.sender, _amount, currentTokenId);
+    }
+
+    function setMockOps(address _ops) external {
+        ops = _ops;
+    }
+
+    function setMockUSDC(address _usdc) external {
+        USDC = _usdc;
+    }
+}
