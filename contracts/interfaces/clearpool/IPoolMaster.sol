2f050df37e76dae958c021f8f47f0b7731d774ab
smartcoding51 <keinakano415@gmail.com>
2022-11-18 04:56:19 +0900

    interface to call provide & withdraw functions

--- contracts/interfaces/clearpool/IPoolMaster.sol
+++ contracts/interfaces/clearpool/IPoolMaster.sol
@@ -1,14 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-/// @notice Interface to interact with the borrower pools of Clearpool Finance
 interface IPoolMaster {
     /// @notice Function is used to provide liquidity for Pool in exchange for cpTokens
     /// @dev Approval for desired amount of currency token should be given in prior
     /// @param currencyAmount Amount of currency token that user want to provide
     function provide(uint256 currencyAmount) external;
 
+    /// @notice Function is used to provide liquidity in exchange for cpTokens to the given address
+    /// @dev Approval for desired amount of currency token should be given in prior
+    /// @param currencyAmount Amount of currency token that user want to provide
+    /// @param receiver Receiver of cpTokens
+    function provideFor(uint256 currencyAmount, address receiver) external;
+
     /// @notice Function is used to redeem previously provided liquidity with interest, burning cpTokens
     /// @param tokens Amount of cpTokens to burn (MaxUint256 to burn maximal possible)
     function redeem(uint256 tokens) external;
+
+    /// @notice Function is used to redeem previously provided liquidity with interest, burning cpTokens
+    /// @param currencyAmount Amount of currency to redeem (MaxUint256 to redeem maximal possible)
+    function redeemCurrency(uint256 currencyAmount) external;
 }
