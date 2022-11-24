16740a4ad1c3f13ec506a5542d4b16b18e115082
smartcoding51 <keinakano415@gmail.com>
2022-11-25 06:01:16 +0900

    add mainnet fork test

---
+++ test/mainnet/product.js
@@ -0,0 +1,201 @@
+const { expect } = require("chai");
+const { ethers, upgrades } = require("hardhat");
+
+const { parseEther, parseUnits } = ethers.utils;
+
+describe("SHFactory test suite", function () {
+    let shFactory, shProduct, shNFT, usdc;
+    let aurosPool, wintermutePool; // clearpool contracts
+    let owner, user1, user2, whaleSigner;
+
+    const whaleAddress = "0xDa9CE944a37d218c3302F6B82a094844C6ECEb17";
+    const qredoWallet = "0xebC37b9cb1657C50676526d28fFfFd54B0A06be2";
+    const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+
+    // Clearpools
+    const aurosPoolAddr = "0x3aeB3a8F0851249682A6a836525CDEeE5aA2A153";
+    const wintermutePoolAddr = "0xcb288b6d30738db7e3998159d192615769794b5b";
+
+    before(async () => {
+        [owner, user1, user2] = await ethers.getSigners();
+    
+        const SHFactory = await ethers.getContractFactory("SHFactory");
+        shFactory = await upgrades.deployProxy(SHFactory, []);
+        await shFactory.deployed();
+    
+        const SHNFT = await ethers.getContractFactory("SHNFT");
+        shNFT = await upgrades.deployProxy(SHNFT, [
+          "Superhedge NFT", "SHN", shFactory.address
+        ]);
+        await shNFT.deployed();
+    
+        usdc = await ethers.getContractAt("IERC20", USDC);
+
+        console.log(usdc.address);
+
+        aurosPool = await ethers.getContractAt(
+            "IPoolMaster",
+            aurosPoolAddr
+        );
+        
+        wintermutePool = await ethers.getContractAt(
+            "IPoolMaster",
+            wintermutePoolAddr
+        );
+
+        // unlock accounts
+        await network.provider.send("hardhat_impersonateAccount", [whaleAddress]);
+        await network.provider.send("hardhat_impersonateAccount", [qredoWallet]);
+
+        // send enough ETH
+        await owner.sendTransaction({
+            to: whaleAddress,
+            value: parseEther("5")
+        });
+
+        whaleSigner = await ethers.provider.getSigner(whaleAddress);
+    });
+
+    describe("Create product", () => {
+        const productName = "BTC Defensive Spread";
+        const issuanceCycle = {
+            coupon: 10,
+            strikePrice1: 25000,
+            strikePrice2: 20000,
+            strikePrice3: 0,
+            strikePrice4: 0,
+            uri: "https://gateway.pinata.cloud/ipfs/QmWsa9T8Br16atEbYKit1e9JjXgNGDWn45KcYYKT2eLmSH"
+        }
+
+        it("Reverts if max capacity is not whole-number thousands", async () => {
+            await expect(
+              shFactory.createProduct(
+                productName,
+                "BTC/USD",
+                usdc.address,
+                owner.address,
+                shNFT.address,
+                qredoWallet,
+                2500,
+                issuanceCycle
+              )
+            ).to.be.revertedWith("Max capacity must be whole-number thousands");
+        });
+
+        it("Successfully created", async () => {
+            expect(await shFactory.createProduct(
+              productName,
+              "BTC/USD",
+              usdc.address,
+              owner.address,
+              shNFT.address,
+              qredoWallet,
+              1000000,
+              issuanceCycle
+            )).to.be.emit(shFactory, "ProductCreated");
+        
+            expect(await shFactory.numOfProducts()).to.equal(1);
+        
+            // get product
+            const productAddr = await shFactory.getProduct(productName);
+            const SHProduct = await ethers.getContractFactory("SHProduct");
+            shProduct = SHProduct.attach(productAddr);
+        
+            expect(await shProduct.currentTokenId()).to.equal(0);
+            expect(await shProduct.shNFT()).to.equal(shNFT.address);
+        });
+    });
+
+    describe("Deposit", () => {
+        before(async() => {
+            await usdc.connect(whaleSigner).transfer(user1.address, parseUnits("10000", 6));
+            await usdc.connect(whaleSigner).transfer(user2.address, parseUnits("10000", 6));
+        });
+
+        it("Reverts if the product status is not 'Accepted'", async () => {
+            await expect(
+              shProduct.connect(user1).deposit(parseUnits("2000", 6))
+            ).to.be.revertedWith("Not accepted status");
+        });
+
+        it("Reverts if the amount is invalid", async () => {
+            await shProduct.whitelist(owner.address);
+            await shProduct.fundAccept();
+
+            await expect(
+                shProduct.connect(user1).deposit(parseUnits("0", 6))
+            ).to.be.revertedWith("Amount must be greater than zero");
+    
+            await expect(
+                shProduct.connect(user1).deposit(parseUnits("1500", 6))
+            ).to.be.revertedWith("Amount must be whole-number thousands");
+    
+            await expect(
+                shProduct.connect(user1).deposit(parseUnits("20000000", 6))
+            ).to.be.revertedWith("Product is full");
+        });
+
+        it("User1 deposits 2000 USDC", async () => {
+            const amount = parseUnits("2000", 6);
+            const supply = 2000 / 1000;
+            await usdc.connect(user1).approve(shProduct.address, amount);
+
+            const currentTokenID = await shProduct.currentTokenId();
+            
+            expect(
+                await shProduct.connect(user1).deposit(amount)
+            ).to.be.emit(shProduct, "Deposit").withArgs(user1.address, amount, currentTokenID, supply);
+
+            expect(
+                await usdc.balanceOf(shProduct.address)
+            ).to.equal(amount);
+
+            expect(
+                await shNFT.balanceOf(user1.address, currentTokenID)
+            ).to.equal(2);
+
+            expect(await shProduct.currentCapacity()).to.equal(amount);
+            expect(await shProduct.numOfInvestors()).to.equal(1);
+        });
+
+        it("set token URI", async () => {
+            const tokenId = await shProduct.currentTokenId();
+            const URI = "https://gateway.pinata.cloud/ipfs/QmWsa9T8Br16atEbYKit1e9JjXgNGDWn45KcYYKT2eLmSH";
+            await shNFT.setTokenURI(tokenId, URI);
+        });
+
+        it("User2 deposits 1000 USDC but it is reverted since fund is locked", async () => {
+            await shProduct.fundLock();
+            const amount2 = parseUnits("1000", 6);
+            await expect(
+              shProduct.connect(user2).deposit(amount2)
+            ).to.be.revertedWith("Not accepted status");
+        });
+    });
+
+    describe("Check coupon & option payout balance, distribute asset", () => {
+        before(async() => {
+            await shProduct.issuance();
+        });
+      
+        it("Check coupon balance after one week", async () => {
+            await shProduct.weeklyCoupon();
+            const userInfo = await shProduct.userInfo(user1.address);
+            console.log(userInfo);
+            const currentTokenID = await shProduct.currentTokenId();
+            const tokenSupply = parseInt(await shNFT.balanceOf(user1.address, currentTokenID));
+            const couponBalance = tokenSupply * 1000 * Math.pow(10, 6) * 10 / 10000;
+            expect(userInfo.coupon).to.equal(couponBalance);
+            console.log(await ethers.provider.getBalance(owner.address));
+        });
+
+        it("Distribute", async () => {
+            await shProduct.distribute(50, [20, 30], [aurosPoolAddr, wintermutePoolAddr]);
+            console.log(await aurosPool.balanceOf(shProduct.address));
+            console.log(await aurosPool.symbol());
+
+            console.log(await wintermutePool.balanceOf(shProduct.address));
+            console.log(await wintermutePool.symbol());
+        });
+    });
+});
