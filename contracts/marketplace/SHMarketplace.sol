de8be18bd7d2e68ce0c2f4b39fb27e118be0c8a6
smartcoding51 <keinakano415@gmail.com>
2022-11-30 03:08:06 +0900

    NFT marketplace, list item function

---
+++ contracts/marketplace/SHMarketplace.sol
@@ -0,0 +1,142 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/utils/introspection/IERC165Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155Upgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+
+interface IAddressRegistry {
+    function tokenRegistry() external view returns (address);
+
+    function priceFeed() external view returns (address);
+}
+
+interface ITokenRegistry {
+    function enabled(address) external view returns (bool);
+}
+
+interface IPriceFeed {
+    function wETH() external view returns (address);
+
+    function getPrice(address) external view returns (int256, uint8);
+}
+
+contract SHMarketplace is OwnableUpgradeable, ReentrancyGuardUpgradeable {
+    using AddressUpgradeable for address payable;
+
+    /// @notice Events for the contract
+    event ItemListed(
+        address indexed owner,
+        address indexed nft,
+        uint256 tokenId,
+        uint256 quantity,
+        address payToken,
+        uint256 pricePerItem,
+        uint256 startingTime
+    );
+
+    /// @notice Structure for listed items
+    struct Listing {
+        uint256 quantity;
+        address payToken;
+        uint256 pricePerItem;
+        uint256 startingTime;
+    }
+
+    bytes4 private constant INTERFACE_ID_ERC1155 = 0xd9b67a26;
+
+    /// @notice Platform fee
+    uint16 public platformFee;
+
+    /// @notice Platform fee receipient
+    address payable public feeReceipient;
+
+    /// @notice NftAddress -> Token ID -> Owner -> Listing item
+    mapping(address => mapping(uint256 => mapping(address => Listing))) public listings;
+
+    /// @notice Address registry
+    IAddressRegistry public addressRegistry;
+
+    modifier notListed(
+        address _nftAddress,
+        uint256 _tokenId,
+        address _owner
+    ) {
+        Listing memory listing = listings[_nftAddress][_tokenId][_owner];
+        require(listing.quantity == 0, "already listed");
+        _;
+    }
+
+    /// @notice Contract initializer
+    function initialize(address payable _feeRecipient, uint16 _platformFee)
+        public
+        initializer
+    {
+        platformFee = _platformFee;
+        feeReceipient = _feeRecipient;
+
+        __Ownable_init();
+        __ReentrancyGuard_init();
+    }
+
+    /// @notice Method for listing NFT
+    /// @param _nftAddress Address of NFT contract
+    /// @param _tokenId Token ID of NFT
+    /// @param _quantity token amount to list (needed for ERC-1155 NFTs, set as 1 for ERC-721)
+    /// @param _payToken Paying token
+    /// @param _pricePerItem sale price for each iteam
+    /// @param _startingTime scheduling for a future sale
+    function listItem(
+        address _nftAddress,
+        uint256 _tokenId,
+        uint256 _quantity,
+        address _payToken,
+        uint256 _pricePerItem,
+        uint256 _startingTime
+    ) external notListed(_nftAddress, _tokenId, _msgSender()) {
+        if (IERC165Upgradeable(_nftAddress).supportsInterface(INTERFACE_ID_ERC1155)) {
+            IERC1155Upgradeable nft = IERC1155Upgradeable(_nftAddress);
+            require(
+                nft.balanceOf(_msgSender(), _tokenId) >= _quantity,
+                "must hold enough nfts"
+            );
+            require(
+                nft.isApprovedForAll(_msgSender(), address(this)),
+                "item not approved"
+            );
+        } else {
+            revert("invalid nft address");
+        }
+
+        _validPayToken(_payToken);
+
+        listings[_nftAddress][_tokenId][_msgSender()] = Listing(
+            _quantity,
+            _payToken,
+            _pricePerItem,
+            _startingTime
+        );
+        emit ItemListed(
+            _msgSender(),
+            _nftAddress,
+            _tokenId,
+            _quantity,
+            _payToken,
+            _pricePerItem,
+            _startingTime
+        );
+    }
+
+    function _validPayToken(address _payToken) internal view {
+        require(
+            _payToken == address(0) ||
+                (addressRegistry.tokenRegistry() != address(0) &&
+                    ITokenRegistry(addressRegistry.tokenRegistry())
+                        .enabled(_payToken)),
+            "invalid pay token"
+        );
+    }
+}
