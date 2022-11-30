a04b9c02b666cff43987e397c9ec8d31dffa93b9
smartcoding51 <keinakano415@gmail.com>
2022-12-01 02:28:54 +0900

    update listing & buy NFT

--- contracts/marketplace/SHMarketplace.sol
+++ contracts/marketplace/SHMarketplace.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/utils/introspection/IERC165Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155Upgradeable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
@@ -26,6 +27,7 @@ interface IPriceFeed {
 
 contract SHMarketplace is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     using AddressUpgradeable for address payable;
+    using SafeERC20Upgradeable for IERC20Upgradeable;
 
     /// @notice Events for the contract
     event ItemListed(
@@ -38,6 +40,25 @@ contract SHMarketplace is OwnableUpgradeable, ReentrancyGuardUpgradeable {
         uint256 startingTime
     );
 
+    event ItemSold(
+        address indexed seller,
+        address indexed buyer,
+        address indexed nft,
+        uint256 tokenId,
+        uint256 quantity,
+        address payToken,
+        int256 unitPrice,
+        uint256 pricePerItem
+    );
+
+    event ItemUpdated(
+        address indexed owner,
+        address indexed nft,
+        uint256 tokenId,
+        address payToken,
+        uint256 newPrice
+    );
+    
     event ItemCanceled(
         address indexed owner,
         address indexed nft,
@@ -86,6 +107,19 @@ contract SHMarketplace is OwnableUpgradeable, ReentrancyGuardUpgradeable {
         _;
     }
 
+    modifier validListing(
+        address _nftAddress,
+        uint256 _tokenId,
+        address _owner
+    ) {
+        Listing memory listedItem = listings[_nftAddress][_tokenId][_owner];
+
+        _validOwner(_nftAddress, _tokenId, _owner, listedItem.quantity);
+
+        require(_getNow() >= listedItem.startingTime, "item not buyable");
+        _;
+    }
+    
     /// @notice Contract initializer
     function initialize(address payable _feeRecipient, uint16 _platformFee)
         public
@@ -153,6 +187,132 @@ contract SHMarketplace is OwnableUpgradeable, ReentrancyGuardUpgradeable {
         isListed(_nftAddress, _tokenId, _msgSender())
     {
         _cancelListing(_nftAddress, _tokenId, _msgSender());
+    }
+
+    /// @notice Method for updating listed NFT
+    /// @param _nftAddress Address of NFT contract
+    /// @param _tokenId Token ID of NFT
+    /// @param _payToken payment token
+    /// @param _newPrice New sale price for each iteam
+    function updateListing(
+        address _nftAddress,
+        uint256 _tokenId,
+        address _payToken,
+        uint256 _newPrice
+    ) external nonReentrant isListed(_nftAddress, _tokenId, _msgSender()) {
+        Listing storage listedItem = listings[_nftAddress][_tokenId][
+            _msgSender()
+        ];
+
+        _validOwner(_nftAddress, _tokenId, _msgSender(), listedItem.quantity);
+
+        _validPayToken(_payToken);
+
+        listedItem.payToken = _payToken;
+        listedItem.pricePerItem = _newPrice;
+        emit ItemUpdated(
+            _msgSender(),
+            _nftAddress,
+            _tokenId,
+            _payToken,
+            _newPrice
+        );
+    }
+
+    /// @notice Method for buying listed NFT
+    /// @param _nftAddress NFT contract address
+    /// @param _tokenId TokenId
+    function buyItem(
+        address _nftAddress,
+        uint256 _tokenId,
+        address _payToken,
+        address _owner
+    )
+        external
+        nonReentrant
+        isListed(_nftAddress, _tokenId, _owner)
+        validListing(_nftAddress, _tokenId, _owner)
+    {
+        Listing memory listedItem = listings[_nftAddress][_tokenId][_owner];
+        require(listedItem.payToken == _payToken, "invalid pay token");
+
+        _buyItem(_nftAddress, _tokenId, _payToken, _owner);
+    }
+
+    ////////////////////////////
+    /// Internal and Private ///
+    ////////////////////////////
+
+    function _buyItem(
+        address _nftAddress,
+        uint256 _tokenId,
+        address _payToken,
+        address _owner
+    ) private {
+        Listing memory listedItem = listings[_nftAddress][_tokenId][_owner];
+
+        uint256 price = listedItem.pricePerItem * listedItem.quantity;
+        uint256 feeAmount = price * platformFee / 1e3;
+
+        IERC20Upgradeable(_payToken).safeTransferFrom(
+            _msgSender(),
+            feeReceipient,
+            feeAmount
+        );
+
+        IERC20Upgradeable(_payToken).safeTransferFrom(
+            _msgSender(),
+            _owner,
+            price - feeAmount
+        );
+
+        // Transfer NFT to buyer
+        IERC1155Upgradeable(_nftAddress).safeTransferFrom(
+            _owner,
+            _msgSender(),
+            _tokenId,
+            listedItem.quantity,
+            bytes("")
+        );
+
+        emit ItemSold(
+            _owner,
+            _msgSender(),
+            _nftAddress,
+            _tokenId,
+            listedItem.quantity,
+            _payToken,
+            getPrice(_payToken),
+            price / listedItem.quantity
+        );
+        delete (listings[_nftAddress][_tokenId][_owner]);
+    }
+    
+    /**
+     @notice Method for getting price for pay token
+     @param _payToken Paying token
+     */
+    function getPrice(address _payToken) public view returns (int256) {
+        int256 unitPrice;
+        uint8 decimals;
+        IPriceFeed priceFeed = IPriceFeed(addressRegistry.priceFeed());
+
+        if (_payToken == address(0)) {
+            (unitPrice, decimals) = priceFeed.getPrice(priceFeed.wETH());
+        } else {
+            (unitPrice, decimals) = priceFeed.getPrice(_payToken);
+        }
+        if (decimals < 18) {
+            unitPrice = unitPrice * (int256(10)**(18 - decimals));
+        } else {
+            unitPrice = unitPrice / (int256(10)**(decimals - 18));
+        }
+
+        return unitPrice;
+    }
+
+    function _getNow() internal view virtual returns (uint256) {
+        return block.timestamp;
     }
 
     function _validPayToken(address _payToken) internal view {
