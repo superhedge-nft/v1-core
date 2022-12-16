48218579e0a8bdfc63797afe42584e638707e025
smartcoding51 <keinakano415@gmail.com>
2022-12-17 02:23:47 +0900

    modify deployment script

---
+++ scripts/testnet/marketplace.js
@@ -0,0 +1,51 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+    // Deploy AddressRegistry contract
+    const AddressRegistry = await ethers.getContractFactory("AddressRegistry");
+    const addressRegistry = await upgrades.deployProxy(AddressRegistry, []);
+    await addressRegistry.deployed();
+
+    console.log(`AddressRegistry deployed at ${addressRegistry.address}`);
+
+    // Deploy PriceFeed contract
+    const wETH = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6";
+    const PriceFeed = await ethers.getContractFactory("PriceFeed");
+    const priceFeed = await upgrades.deployProxy(PriceFeed, [
+        addressRegistry.address, wETH
+    ]);
+    await priceFeed.deployed();
+
+    console.log(`PriceFeed deployed at ${priceFeed.address}`);
+
+    // Deploy TokenRegistry contract
+    const TokenRegistry = await ethers.getContractFactory("TokenRegistry");
+    const tokenRegistry = await upgrades.deployProxy(TokenRegistry, []);
+    await tokenRegistry.deployed();
+
+    console.log(`TokenRegistry deployed at ${tokenRegistry.address}`);
+
+    const platformFee = 5; // 0.5% of sales price
+    const feeRecipient = "0x6Ca8304ae1973C205c6ac9A6Fb82a017cA800e77";
+
+    const SHMarketplace = await ethers.getContractFactory("SHMarketplace");
+    const shMarketplace = await upgrades.deployProxy(SHMarketplace, [
+        feeRecipient, platformFee
+    ]);
+    await shMarketplace.deployed();
+
+    console.log(`SHMarketplace deployed at ${shMarketplace.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
---
+++ scripts/testnet/product.js
@@ -0,0 +1,65 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+    // Deploy factory contract
+    const SHFactory = await ethers.getContractFactory("SHFactory");
+    const shFactory = await upgrades.deployProxy(SHFactory, []);
+    await shFactory.deployed();
+
+    console.log(`SHFactory deployed at ${shFactory.address}`);
+
+    // Deploy NFT contract
+    const SHNFT = await ethers.getContractFactory("SHNFT");
+    const shNFT = await upgrades.deployProxy(SHNFT, [
+        "Superhedge NFT", "SHN", shFactory.address
+    ]);
+    await shNFT.deployed();
+
+    console.log(`SHNFT deployed at ${shNFT.address}`);
+
+    const usdc = "0x07865c6E87B9F70255377e024ace6630C1Eaa37F";
+
+    const manager = "0x6Ca8304ae1973C205c6ac9A6Fb82a017cA800e77";
+    const qredoWallet = "0xbba1088BD130AF05AA0ab3EA89464F10C83B984A"; // Qredo Metamask Institutional
+
+    const productName = "BTC Defensive Spread"
+    const issuanceCycle = {
+        coupon: 10,
+        strikePrice1: 25000,
+        strikePrice2: 20000,
+        strikePrice3: 0,
+        strikePrice4: 0,
+        uri: "https://gateway.pinata.cloud/ipfs/QmWsa9T8Br16atEbYKit1e9JjXgNGDWn45KcYYKT2eLmSH"
+    }
+
+    // Create new product
+    const tx = await shFactory.createProduct(
+        productName, // product name
+        "BTC/USD", // underlying
+        usdc, // USDC address on Goerli testnet
+        manager,
+        shNFT.address, // ERC1155 NFT address
+        qredoWallet, // QREDO Wallet
+        1000000, // Max capacity
+        issuanceCycle // First issuance cycle
+    );
+  
+    await tx.wait();
+  
+    const productAddr = await shFactory.getProduct(productName);
+
+    console.log(`SHProduct deployed at ${productAddr}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
