77849a9845bab2cd79c87a3ee796046e888ba17f
smartcoding51 <keinakano415@gmail.com>
2023-05-12 01:05:12 +0900

    redeploy marketplace contract

--- test/mainnet/marketplace.js
+++ test/mainnet/marketplace.js
@@ -9,7 +9,7 @@ describe("SHMarketplace test suite", () => {
     let owner, feeRecipient;
 
     const platformFee = 5; // 0.5% of sales price
-    const wETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+    const wETH = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1";
 
     before(async() => {
         [owner, feeRecipient, user1, user2] = await ethers.getSigners();
@@ -183,6 +183,9 @@ describe("SHMarketplace test suite", () => {
                 startingTime,
                 listingId
             );
+
+            const listingCount = await shMarketplace.listingCount(user1.address, currentTokenID);
+            expect(listingCount).to.equal(quantity1);
         });
 
         it("Successfully updates item", async() => {
@@ -199,7 +202,7 @@ describe("SHMarketplace test suite", () => {
             );
         });
 
-        it("Create another listing and cancel listing", async() => {
+        it("Create another listing", async() => {
             listingId = await shMarketplace.nextListingId();
 
             expect(await shMarketplace.connect(user1).listItem(
@@ -221,6 +224,9 @@ describe("SHMarketplace test suite", () => {
                 startingTime,
                 listingId
             );
+
+            const listingCount = await shMarketplace.listingCount(user1.address, currentTokenID);
+            expect(listingCount).to.equal(quantity1 + quantity2);
         });
 
         it("Cancel listing", async() => {
@@ -230,6 +236,9 @@ describe("SHMarketplace test suite", () => {
                 user1.address,
                 listingId
             );
+
+            const listingCount = await shMarketplace.listingCount(user1.address, currentTokenID);
+            expect(listingCount).to.equal(quantity1);
         });
     });
 
@@ -310,6 +319,9 @@ describe("SHMarketplace test suite", () => {
             listing = await shMarketplace.listings(listingId);
             
             expect(listing.listingId).to.equal(0);
+
+            const listingCount = await shMarketplace.listingCount(user1.address, currentTokenID);
+            expect(listingCount).to.equal(0);
         });
     });
 });
