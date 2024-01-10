fb8c755efebbb19415cfce8f4984df420cb67cf1
smartcoding51 <keinakano415@gmail.com>
2024-01-11 04:32:08 +0900

    add Lido interfaces

--- contracts/interfaces/lido/ILido.sol
+++ contracts/interfaces/lido/ILido.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+
 interface ILido {
     /**
      * @notice Send funds to the pool with optional _referral parameter
@@ -8,4 +9,12 @@ interface ILido {
      * @return Amount of StETH shares generated
      */
     function submit(address _referral) external payable returns (uint256);
+
+    /**
+     * @return the amount of tokens owned by the `_account`.
+     *
+     * @dev Balances are dynamic and equal the `_account`'s share in the amount of the
+     * total Ether controlled by the protocol. See `sharesOf`.
+     */
+    function balanceOf(address _account) external view returns (uint256);
 }
